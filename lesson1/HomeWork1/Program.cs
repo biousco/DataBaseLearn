ab5e4100eab42a228afcdd9177128e385ffebed5
Biousco <441936245@qq.com>
2015-09-11 16:03:59 +0800

    第一节课

---
+++ lesson1/HomeWork1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("甲方记住牌照号码前两位数字相同");
+            Console.WriteLine("乙方记住牌照号码后两位数字相同");
+            Console.WriteLine("丙方说车牌号码四位数，恰好是一个整数的平方");
+            //平方为4位数的取值范围为32~99
+            for(int i = 32; i < 100; i++)
+            {
+                int first = getMod(i * i);
+                if(first >= 0)
+                {
+                    int last = getMod(first);
+                    if(last >= 0)
+                    {
+                        Console.WriteLine("我夜观天象，发现车牌号码是：" + i * i);
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("...");
+                        continue;
+                    }
+                } else
+                {
+                    continue;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static int getMod(int num)
+        {
+            int fir1 = num % 10;
+            int temp = num / 10;
+            int fir2 = temp % 10;
+            if(fir1 != fir2)
+            {
+                return -1;
+            } else
+            {
+                return temp / 10;
+            }
+        }
+    }
+}
---
+++ lesson1/test2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入你的姓名：");
+            String name = Console.ReadLine();
+            Console.WriteLine("欢迎你，" + name + "!");
+            Console.WriteLine("按下回车键退出...");
+            Console.ReadLine();
+        }
+    }
+}
