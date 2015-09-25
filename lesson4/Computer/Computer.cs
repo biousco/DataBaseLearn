66e14a0239bd691f9132dba15378b19d7d2843be
Biousco <441936245@qq.com>
2015-09-25 11:06:06 +0800

    第四次课-first

---
+++ lesson4/Computer/Computer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer
+{
+    class Computer
+    {
+        public double firstNum, secondNum;
+        public Char operate;
+
+        public void setFirstNum(double Num)
+        {
+
+            this.firstNum = Num;
+        }
+        public void setSecondNum(double Num)
+        {
+            this.secondNum = Num;
+        }
+
+        public void compute ()
+        {
+            switch(operate)
+            {
+                case '+':
+                {
+                        Console.WriteLine(firstNum + secondNum);
+                        break;
+                }
+                case '-':
+                    {
+                        Console.WriteLine(firstNum - secondNum);
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.WriteLine(firstNum * secondNum);
+                        break;
+                    }
+                case '/':
+                    {
+                        if(secondNum == 0)
+                        {
+                            Console.WriteLine("数值不合法");
+                            break;
+                        } else
+                        {
+                            Console.WriteLine(firstNum / secondNum);
+                        }
+                        break;
+                    }
+            }
+
+        }
+    }
+}
---
+++ lesson4/Computer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer();
+            computer.firstNum = 15;
+            computer.secondNum = 20;
+            computer.operate = '*';
+            computer.compute();
+            Console.ReadLine();
+        }
+    }
+}
