4ead34e10476a004facb00a087ae59f2b36df25f
Biousco <441936245@qq.com>
2015-10-31 21:13:58 +0800

    数据库一

---
+++ lesson6/windowsConnection/UserConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windowsConnection
+{
+    class UserConnection
+    {
+        public static void Main2()
+        {
+            string connStr = "Server=.\\SQLEXPRESS;database=study;User ID=ml;Password=111;Trusted_Connection=False";
+            SqlConnection conn;
+            using (conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("数据库已连接!通过Sql用户名");
+                    Console.WriteLine("\nDataSource:{0}", conn.DataSource);
+                    Console.WriteLine("Database:{0}", conn.Database);
+                    Console.WriteLine("ConnectionTimeOut:{0}", conn.ConnectionTimeout);
+                }
+            }
+            if (conn.State == ConnectionState.Closed)
+            {
+                Console.WriteLine("\n数据库连接已关闭!");
+            }
+        }
+    }
+}
---
+++ lesson6/windowsConnection/WindowsConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windowsConnection
+{
+    class WindowsConnection
+    {
+        static void Main(string[] args)
+        {
+            UserConnection.Main2();
+            string connStr = "Server=.\\SQLEXPRESS;Initial Catalog=study;Integrated Security=SSPI";
+
+            SqlConnection conn = new SqlConnection(connStr);
+            try
+            {
+                conn.Open();
+
+                if(conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("数据库已连接！通过Windows验证！");
+                    Console.WriteLine("\nDataSource: {0}", conn.DataSource);
+                    Console.WriteLine("Database: {0}", conn.Database);
+                    Console.WriteLine("ConnectionTimeout: {0}", conn.ConnectionTimeout);
+                    conn.Close();
+                }
+            }catch
+            {
+                Console.WriteLine("数据库连接失败");
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+            if(conn.State == ConnectionState.Closed)
+            {
+                Console.WriteLine("\n数据库连接关闭");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
