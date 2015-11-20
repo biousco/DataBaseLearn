16a4a4dfd8ea8a9aff56f3167d309ef7505e678e
Biousco <441936245@qq.com>
2015-11-20 11:20:01 +0800

    实现接口

---
+++ lesson9/Exp_2_访问Access的通讯录系统/IDAL/IContactGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    public interface IContactGroup
+    {
+        DataTable GetList(string strWhere);
+        bool Add(Model.ContactGroup model);
+        Model.ContactGroup GetModel(int Id);
+        Model.ContactGroup GetModel(string groupName);
+        bool Delete(int Id);
+        bool Update(Model.ContactGroup model);
+    }
+}
---
+++ lesson9/Exp_2_访问Access的通讯录系统/IDAL/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    public interface IUser
+    {
+        //判断用户名、密码是否正确
+        bool Login(string userName, string userPassword);
+        //
+        bool Update(Model.User model);
+    }
+}
